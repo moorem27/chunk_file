68fbd7e952f03aec6807c58e87f1aa4dbcc4199e
mmoore <moorem27@uw.edu>
2017-12-18 20:53:30 -0800

    Get rid of threads

--- chunk.cc
+++ chunk.cc
@@ -1,18 +1,13 @@
-#include <string.h>
+#include <cstring>
 #include <vector>
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <chrono>
-#include <stdio.h>
-#include <utility>
-#include <thread>
 
-// TODO Do more error checking all around
 namespace {
-    // Adjust this for performance as needed
-	const int MAX_BUFFER_SIZE = 4096;
-    int FILE_NUMBER = 1;
+    const int MAX_BUFFER_SIZE = 4096;
+    char buffer[ MAX_BUFFER_SIZE ];
 }
 
 /**
@@ -23,7 +18,7 @@ namespace {
  */
 std::string get_extension( const std::string& file_path ) {
     if( !file_path.empty() ) {
-        size_t last_index = file_path.find_last_of( "." );
+        size_t last_index = file_path.find_last_of( '.' );
         std::string extension = file_path.substr( last_index, file_path.length() );
         return extension;
     } else {
@@ -39,7 +34,7 @@ std::string get_extension( const std::string& file_path ) {
  */
 std::string remove_extension( const std::string& file_path ) {
     if( !file_path.empty() ) {
-        size_t dot = file_path.find_last_of( "." );
+        size_t dot = file_path.find_last_of( '.' );
         if ( dot == std::string::npos ) return file_path;
         return file_path.substr( 0, dot );
     } else {
@@ -54,82 +49,36 @@ std::string remove_extension( const std::string& file_path ) {
  */
 std::string clone_name( const std::string& file_path ) {
     if( !file_path.empty() )
-	    return remove_extension( file_path ) + "_clone" + get_extension( file_path );
+        return remove_extension( file_path ) + "_clone" + get_extension( file_path );
     else
         return "";
 }
 
-std::vector<std::pair<long long int, long long int>> create_chunk_pairs( const int num_chunks, const long long int file_size ) {
-    std::vector<std::pair<long long int, long long int>> chunk_pairs{};
-
-    long long int current = 0;
-    
-    std::cout << "FILE SIZE: " << file_size << std::endl;
-    for ( int i = 1; i <= num_chunks; ++i ) {
-        long long int new_end = ( ( i * file_size ) / num_chunks );
-        
-        chunk_pairs.emplace_back( std::make_pair( current, new_end ) );
-        current = new_end;
-    }
-
-    return chunk_pairs;
-}
-
-
-std::string create_file_chunk( std::pair<long long int, long long int> pair, 
-                               const std::string file_path,
-                               const long long int file_size,
-                               const int file_number  ) {
-    std::ifstream file{};
-    const std::string extension = get_extension( file_path );
-    const std::string file_name = remove_extension( file_path );
-	char buffer[ MAX_BUFFER_SIZE ];
-    long long int last_byte_read = pair.first;
-
-    // Open file
-    file.open( file_path, std::ios_base::binary );
-
-    // Seek to calculated position
-    file.seekg( pair.first );
-    std::ostringstream out_file_path{};
-    out_file_path << file_name << file_number << extension;
-    std::string out_file_name( out_file_path.str() );
-    FILE* outfile = fopen( out_file_name.c_str(), "wb" );
-    if( outfile && file.good() ) {
-        while( last_byte_read < pair.second  && pair.second <= file_size ) {
-            long long int bytes = 0;
-            if( last_byte_read + MAX_BUFFER_SIZE <= pair.second ) {
-                bytes = MAX_BUFFER_SIZE;
-            } else if( last_byte_read + MAX_BUFFER_SIZE > pair.second ) {
-                bytes = pair.second - last_byte_read;
-            }
-            file.read( buffer, bytes );
-            fwrite( buffer, static_cast<size_t>( bytes ), 1, outfile );
-            memset( &buffer, 0, static_cast<size_t>( bytes ) );
-            last_byte_read += bytes;
+/**
+ * Iterate over a vector of strings that contain file paths and remove all files
+ * @param paths - A vector of strings representing file paths
+ */
+void erase_chunks( const std::vector<std::string>& paths ) {
+    if( !paths.empty() ) {
+        for ( const auto &path : paths ) {
+            std::remove( path.c_str() );
         }
     }
-    if( outfile )
-        fclose( outfile );
-    
-    file.close();
-    return out_file_name;
 }
 
 
 /**
+ * TODO: This monstrosity needs a cleaning
  * This function splits a file into separate chunks. The file chunks are stored in the
  * same directory as the original file.
  * @param num_chunks - The number of chunks to split the file specified by file_path
  * @param file_path - The file path to the file
  * @return A vector of strings containing the file paths to the newly created chunks
  */
-void create_file_chunks( const int num_chunks, const std::string& file_path ) {
+std::vector<std::string> create_file_chunks( const long num_chunks, const std::string& file_path ) {
     if( !file_path.empty() && num_chunks > 0 ) {
         std::ifstream file{};
         std::vector<std::string> paths{};
-        std::vector<std::thread> threads{};
-
         const std::string extension = get_extension( file_path );
         const std::string file_name = remove_extension( file_path );
 
@@ -141,6 +90,11 @@ void create_file_chunks( const int num_chunks, const std::string& file_path ) {
 
         // Grab total file size
         const long long int file_size = file.tellg();
+        
+        if( num_chunks > file_size ) {
+            std::cout << "Sorry, the number of chunks cannot be greater than the file size" << std::endl;
+            return {};
+        }
 
         // Seek back to beginning;
         file.seekg( 0, std::ifstream::beg );
@@ -148,17 +102,48 @@ void create_file_chunks( const int num_chunks, const std::string& file_path ) {
         // The last amount of bytes read
         long long int last_byte_read = 0;
 
-        auto pairs = create_chunk_pairs( num_chunks, file_size );
-        
-        file.close();
+        // For each chunk, do:
+        for ( int i = 1; i <= num_chunks; ++i ) {
+            memset( &buffer, 0, sizeof( buffer ) );
 
-        for( const auto& pair : pairs ) {
-            threads.emplace_back( std::thread( create_file_chunk, pair, file_path, file_size, FILE_NUMBER ) );
-            FILE_NUMBER++;
+            // Calculate the ending byte of the current chunk
+            long long int current_chunk_last_byte = ( ( i * file_size ) / num_chunks );
+
+            // Create chunk file name
+            std::ostringstream out_file_path;
+            out_file_path << file_name << i << extension;
+            std::string out_file_name( out_file_path.str() );
+
+            FILE* outfile = nullptr;
+            outfile = fopen( out_file_name.c_str(), "wb" );
+            if ( outfile && file.good() ) {
+                paths.push_back( out_file_name );
+                while( last_byte_read < current_chunk_last_byte && current_chunk_last_byte <= file_size ) {
+                    long long int bytes = 0;
+                    if( last_byte_read + MAX_BUFFER_SIZE <= current_chunk_last_byte ) {
+                        bytes = MAX_BUFFER_SIZE;
+                    } else if( last_byte_read + MAX_BUFFER_SIZE > current_chunk_last_byte ) {
+                        bytes = current_chunk_last_byte - last_byte_read;
+                    }
+                    file.read( buffer, bytes );
+                    fwrite( buffer, static_cast<size_t>( bytes ), 1, outfile );
+                    memset( &buffer, 0, static_cast<size_t>( bytes ) );
+                    last_byte_read += bytes;
+                }
+            } else {
+                std::cout << "Bad input or output file" << std::endl;
+                return {};
+            }
+            fclose( outfile );
         }
-
-        for( auto& thread : threads )
-            thread.join();
+        if( last_byte_read == file_size ) {
+            std::cout << "Successfully split " << file_size << " bytes into " << num_chunks << " chunks" << std::endl;
+            return paths;
+        } else {
+            return {};
+        }
+    } else {
+        return {};
     }
 }
 
@@ -186,26 +171,56 @@ void create_file_from_chunks( const std::vector<std::string>& paths, const std::
     }
 }
 
+/**
+ * A function to test all of the above code
+ * @param file_path - The path to the file that will be split
+ * @param chunks - The number of chunks to split the file into
+ * @return - Return 0 for success, -1 on error
+ */
+int test_chunks( const std::string& file_path, const unsigned int chunks ) {
+    if( !file_path.empty() && chunks > 0 ) {
+        auto begin = std::chrono::high_resolution_clock::now();
+        std::vector<std::string> paths = create_file_chunks( chunks, file_path );
+        auto end = std::chrono::high_resolution_clock::now();
+        std::cout << "File chunking took: " << std::chrono::duration_cast<std::chrono::milliseconds>( end - begin ).count() << " ms" << '\n';
+        std::cout << "                    " << std::chrono::duration_cast<std::chrono::seconds>( end - begin ).count() << " s" << '\n';
+
+        std::string new_name = clone_name( file_path );
+        if( !paths.empty() )
+            create_file_from_chunks( paths, new_name );
+        return 0;
+    }
+
+    return -1;
+}
+
+
+// TODO Handle files that have no '.'
 
 /**
- *
- * @param argc
- * @param argv
- * @return
+ * Usage:
+ * ./chunk file 4
+ * @param argc the argument count
+ * @param argv[1] is /path/to/file
+ * @param argv[2] is the number of chunks to split file into 
+ * @return 0 if successful
  */
 int main( int argc, char* argv[] ) {
+    constexpr int base = 10;
+    char* end_pointer = nullptr;
     std::string path = argv[ 1 ];
-    const unsigned int chunks = static_cast<unsigned int>( atoi( argv[ 2 ] ) );
+
+    auto chunks = strtol( argv[ 2 ], &end_pointer, base );
+
+    std::cout << "Path: " << std::endl;
     std::cout << path << std::endl;
-   
+
     auto begin = std::chrono::high_resolution_clock::now();
-     
     create_file_chunks( chunks, path );
-    
     auto end = std::chrono::high_resolution_clock::now();
-    std::cout << "File chunking took: " << std::chrono::duration_cast<std::chrono::milliseconds>( end - begin ).count()
-              << " ms" << std::endl;
-    
+
+    std::cout << "File chunking took: " << std::chrono::duration_cast<std::chrono::milliseconds>( end - begin ).count() << " ms" << '\n';
+
     return 0;
 }
 
